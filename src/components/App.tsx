9a3572aedd9c8fa88c559762e8d41e1f9d02f711
Sam Park <samp1800@live.com>
2024-11-04 00:07:21 -0500

    project rename

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,10 +4,10 @@ function App() {
 
   return (
     <>
-      <div className='container'>
-        <div className='row'>
+      <div className='container-fluid'>
+        <div className='row mt-3'>
           <div className='col-12 text-center'>
-            <h3>lyra</h3>
+            <h3>constellations</h3>
           </div>
         </div>
         
