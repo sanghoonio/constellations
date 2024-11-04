04e415f6847947bdd1afb3fb90f3db88d3e77d86
Sam Park <samp1800@live.com>
2024-11-03 20:09:13 -0500

    init vite react

---
+++ src/components/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-12 text-center'>
+            <h3>lyra</h3>
+          </div>
+        </div>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
